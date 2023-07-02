880a40f75bf53528ba91b2bd1b6b3148996f99b1
Tetiana2002 <125501735+Tetiana2002@users.noreply.github.com>
2023-07-02 20:20:30 +0200

    done all task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+const currentTimeKey = "videoplayer-current-time";
+const player = document.querySelector("iframe");
+const videoPlayer = new Player(player, {
+    loop: true,
+    fullscreen: true,
+    quality: "1080p"
+});
+const currentTime = function (currentTime) {
+    const seconds = currentTime.seconds;
+    localStorage.setItem(currentTimeKey,JSON.stringify(seconds))
+}
+videoPlayer.setCurrentTime(localStorage.getItem(currentTimeKey) || 0);
+videoPlayer.on("timeupdate", throttle(currentTime, 1000));
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
-
+import throttle from "lodash.throttle";
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    input: document.querySelector("[name = 'email']"),
+    textarea:document.querySelector("[name = 'message']")
+}
+refs.form.addEventListener("submit", onFormSubmit);
+refs.form.addEventListener("input", throttle(onFormInput, 500));
+populateTextarea();
+function checkForm() {
+    if (refs.input.value === "" || refs.textarea.value === "") {
+       return alert("Всі поля повинні бути заповнені")
+   }
+}
+function onFormSubmit(e) {
+    e.preventDefault();
+    checkForm();
+    localStorage.removeItem("feedback-form-state");
+    e.currentTarget.reset()
+}
+function onFormInput(e) {
+    try {
+        const {
+            elements: { email, message },
+        } = e.currentTarget;
+        const dataUser = {
+            email: email.value,
+            message: message.value
+        };
+        localStorage.setItem("feedback-form-state",JSON.stringify(dataUser))
+        
+    } catch (error) {
+        return; 
+    }
+    
+}
+function populateTextarea() {
+    const {
+            elements: { email, message },
+    } = refs.form;
+    if (localStorage.getItem("feedback-form-state")) {
+        const savedMessage = JSON.parse(localStorage.getItem("feedback-form-state"));
+        email.value = savedMessage.email;
+        message.value = savedMessage.message;
+    }
+}
